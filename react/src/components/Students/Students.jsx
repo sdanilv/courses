d1885c635126f71e6c60f35d6c267a5a62d799ad
sdanilv <sdanlv@gmail.com>
2020-02-05 23:10:33 +0200

    Edit student

--- react/src/components/Students/Students.jsx
+++ react/src/components/Students/Students.jsx
@@ -1,33 +1,39 @@
 import React, {useEffect} from "react";
 import {connect} from "react-redux";
 import {addStudentToServer, getStudentFromServer} from "../../redux/Students/StudentsReducer";
-import AddStudentForm from "../AddStudentForm/AddStudentForm";
+import AddStudentForm from "../FormTools/AddStudentForm";
+import {reset} from "redux-form";
+import TableCell from '@material-ui/core/TableCell';
+import TableRow from '@material-ui/core/TableRow';
+import GenerateTable from "../FormTools/GenerateTable";
 
 const Students = props => {
-    const {getStudentFromServer}  = props;
-    console.log(props);
+    const {getStudentFromServer} = props;
+    const submit = (formValues) => {
+        props.addStudentToServer({...formValues});
+        props.reset("addForm")
+    };
+
     useEffect(() => {
         getStudentFromServer();
     }, [getStudentFromServer]);
-    const submit = (formValues)=>{
 
-props.addStudentToServer({...formValues})
-    };
 
     const students = props.students.map(student => {
-        return <div key={student._id}>
-            <div>Name: {student.name}</div>
-            <div> Age: {student.age}</div>
-            <div> averageMark: {student.averageMark}</div>
-        </div>
+        return <TableRow key={student._id}>
+            <TableCell>{student.name}</TableCell>
+            <TableCell>{student.telephone}</TableCell>
+            <TableCell>{student.email}</TableCell>
+            <TableCell>{student._id}</TableCell>
+        </TableRow>
     });
 
     return <div>
-        <AddStudentForm onSubmit = {submit}/>
-        {students}
+        <AddStudentForm onSubmit={submit}/>
+        <GenerateTable body={students} columnsNames={["Name", "Telephone", "Email", "Action"]}/>
     </div>
 };
 const mapStateToProps = state => ({
     students: state.Students.students
 });
-export default connect(mapStateToProps, {getStudentFromServer, addStudentToServer})(Students);
+export default connect(mapStateToProps, {getStudentFromServer, addStudentToServer ,reset})(Students);
