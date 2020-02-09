f4ec05733bb53f697a5dc7edd633428dd44ed1ae
sdanilv <sdanlv@gmail.com>
2020-02-09 23:47:12 +0200

    + loader to app

--- react/src/App.js
+++ react/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 import {Route, BrowserRouter} from "react-router-dom"
 
@@ -6,17 +6,26 @@ import Students from "./components/Students/Students";
 import Courses from "./components/Courses/Courses";
 import Header from "./components/Header/Header";
 import Course from "./components/Courses/Course/Course";
+import {getCoursesFromServer} from "./redux/Courses/CoursesReducer";
+import {getStudentFromServer} from "./redux/Students/StudentsReducer";
+import {connect} from "react-redux";
+import CircularProgress from "@material-ui/core/CircularProgress";
 
 
-function App() {
-
+function App({getCoursesFromServer, getStudentFromServer}) {
+    const [loading, isLoading] = useState(true);
+    useEffect(() => {
+        getCoursesFromServer().then(() => getStudentFromServer().then(isLoading(false)));
+    }, [getCoursesFromServer, getStudentFromServer]);
     return (
         <BrowserRouter>
-            <Header />
-            <Route exact path="/students" component={Students}/>
-            <Route exact path="/courses" component={Courses}/>
-            <Route  path="/courses/:id" component={Course}/>
+            <Header/>
+            {loading ? <CircularProgress/> : <>
+                <Route exact path="/students" component={Students}/>
+                <Route exact path="/courses" component={Courses}/>
+                <Route path="/courses/:id" component={Course}/></>}
         </BrowserRouter>);
 }
 
-export default App;
+const mapStateToProps = state => ({});
+export default connect(mapStateToProps, {getCoursesFromServer, getStudentFromServer})(App);
